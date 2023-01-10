910465be08a39dbfcf321916dc4762735ea168ca
SomendraZ <somendra.swaroop@kalvium.community>
2023-01-10 16:57:53 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import useStorage from "../hooks/useStorage";
+
+const Home = () => {
+  let [userInput, updateInput] = useStorage("");
+
+  return (
+    <div className="Home">
+      <input
+        type="text"
+        onChange={(e) => updateInput(e.target.value)}
+        value={userInput ? userInput : ""}
+        placeholder="Type something"
+      />
+    </div>
+  );
+};
+
+export default Home;
